8ce1fd6923cd17f6d4f542f0d6a982af48722767
subrata410 <subrata.rooy2022@gmail.com>
2022-12-20 01:22:39 +0600

    JavaScript class 1,2

---
+++ script.js
@@ -0,0 +1,74 @@
+// let life = "Beautifull";
+// if (life === "Beautifull") alert("Life is Beautifull");
+
+// console.log(6 + 9 + 10);
+// let firstName = "subrata";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(18 - 12);
+// let profit = 32 - 12;
+// console.log(profit);
+// let myAge = 2022 - 1998;
+// console.log(myAge);
+// myAge = 2022 - 1990;
+// console.log(myAge);
+// let currenYear = 2023;
+// let myAge = currenYear - 1998;
+// console.log(myAge);
+// let nituAge = currenYear - 2015;
+// console.log(nituAge);
+// console.log(myAge, nituAge);
+// let ageDiff = myAge - nituAge;
+// console.log(ageDiff);
+
+
+// +
+// -
+// *
+// /
+// **
+// %
+// ++
+// --
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(5 * 5);
+// console.log(25 / 5);
+// console.log(3 ** 3);
+// console.log(26 % 3);
+
+// let x = 10;
+// x++;
+// console.log(x);
+// let y = 10;
+// y--;
+// console.log(y);
+
+// let firstName = "Subrata";
+// let lastName = "Nitu";
+
+// console.log(firstName + " " + lastName);
+
+// =
+// +=
+// -=
+// *=
+// /=
+// let item = 10 + 5;
+// console.log(item);
+
+// let x = 15;
+// x += 10; // x = x + 10
+// console.log(x);
+// let y = 20;
+// y -= 15; //y = y -15
+// console.log(y);
+// let z = 5;
+// z *= 5; // z = z*5
+// console.log(z);
+// let w = 15;
+// w /= 3; // w= w / 3;
+// console.log(w);  
